1e825f52678316e2e35f6c3ac729b73e249608b2
oxteddybear <thebrownteddybear@gmail.com>
2021-07-07 14:06:22 +0800

    copy folders

---
+++ workflow2/main.tf
@@ -0,0 +1,132 @@
+provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
+
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "target_dc" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_datastore" "target_datastore" {
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.target_dc.id
+}
+
+data "vsphere_compute_cluster" "target_cluster" {
+  name          = var.vsphere_cluster
+  datacenter_id = data.vsphere_datacenter.target_dc.id
+}
+
+data "vsphere_network" "mgt_network" {
+  name          = var.mgt_network
+  datacenter_id = data.vsphere_datacenter.target_dc.id
+}
+data "vsphere_network" "iscsi_network1" {
+  name          = var.iscsi_network1
+  datacenter_id = data.vsphere_datacenter.target_dc.id
+}
+data "vsphere_network" "iscsi_network2" {
+  name          = var.iscsi_network2
+  datacenter_id = data.vsphere_datacenter.target_dc.id
+}
+
+data "vsphere_network" "workload_network" {
+  name          = var.workload_network
+  datacenter_id = data.vsphere_datacenter.target_dc.id
+}
+
+data "vsphere_virtual_machine" "source_template" {
+  name          = var.guest_template
+  datacenter_id = data.vsphere_datacenter.target_dc.id
+}
+
+
+
+resource "vsphere_virtual_machine" "vesxi" {
+  for_each = var.vm_names
+  name = each.key
+  datastore_id     = data.vsphere_datastore.target_datastore.id
+  folder           = var.vsphere_folder
+  resource_pool_id = data.vsphere_compute_cluster.target_cluster.resource_pool_id
+  num_cpus = var.guest_vcpu
+  memory   = var.guest_memory
+  nested_hv_enabled = "true"
+  wait_for_guest_net_routable ="false"
+  guest_id = data.vsphere_virtual_machine.source_template.guest_id
+  wait_for_guest_net_timeout = 35
+  wait_for_guest_ip_timeout = 35
+  scsi_type = data.vsphere_virtual_machine.source_template.scsi_type
+
+
+  # First interface will be in MGMT port group
+  network_interface {
+    network_id   = data.vsphere_network.mgt_network.id
+    adapter_type = data.vsphere_virtual_machine.source_template.network_interface_types[0]
+  }
+  
+ network_interface {
+    network_id   = data.vsphere_network.mgt_network.id
+    adapter_type = data.vsphere_virtual_machine.source_template.network_interface_types[0]
+  }
+  network_interface {
+    network_id   = data.vsphere_network.iscsi_network1.id
+    adapter_type = data.vsphere_virtual_machine.source_template.network_interface_types[0]
+  }
+  network_interface {
+    network_id   = data.vsphere_network.iscsi_network2.id
+    adapter_type = data.vsphere_virtual_machine.source_template.network_interface_types[0]
+  }
+    network_interface {
+    network_id   = data.vsphere_network.workload_network.id
+    adapter_type = data.vsphere_virtual_machine.source_template.network_interface_types[0]
+  }
+  network_interface {
+    network_id   = data.vsphere_network.workload_network.id
+    adapter_type = data.vsphere_virtual_machine.source_template.network_interface_types[0]
+  }
+ 
+ network_interface {
+    network_id   = data.vsphere_network.workload_network.id
+    adapter_type = data.vsphere_virtual_machine.source_template.network_interface_types[0]
+  }
+  network_interface {
+    network_id   = data.vsphere_network.workload_network.id
+    adapter_type = data.vsphere_virtual_machine.source_template.network_interface_types[0]
+  }
+ 
+  disk {
+    label            = "disk0"
+    size             = var.guest_disk0_size
+    thin_provisioned = true
+  }
+  
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.source_template.id
+    timeout = 120     
+  
+  }
+
+provisioner "remote-exec" {
+    inline = ["esxcli system hostname set -H=${each.key} -d=${var.guest_domain}",
+    "esxcli network ip dns server add --server=${var.guest_dns}",
+    "echo server ${var.guest_ntp} > /etc/ntp.conf && /etc/init.d/ntpd start",
+    "esxcli network vswitch standard uplink add --uplink-name=vmnic1 --vswitch-name=vSwitch0",
+    "esxcli network ip interface ipv4 set -i vmk0 -t static -g ${var.guest_gateway} -I ${var.guest_start_ip}${each.value} -N ${var.guest_netmask} ",
+    ]
+}
+
+connection  {
+      user           = var.guest_user
+      password       = var.guest_password
+      timeout = 15
+      host  = self.guest_ip_addresses[0]
+    }  
+
+   }
+
+
+  
---
+++ workflow2/varables.tf
@@ -0,0 +1,106 @@
+
+variable "vsphere_server" {
+  description = "vCenter FQDN/IP "
+}
+
+variable "guest_template" {
+  description = "vsphere template"
+}
+variable "vsphere_user" {
+  description = "vSphere username"
+}
+
+variable "vsphere_password" {
+  description = "vSphere password"
+}
+
+variable "vsphere_datacenter" {
+  description = "vSphere datacenter"
+}
+
+variable "vsphere_cluster" {
+  description = "vSphere cluster"
+}
+
+variable "vsphere_datastore" {
+  description = "Datastore where VMs will be deployed."
+}
+
+variable "vsphere_folder" {
+  description = "vSphere folder to store VMs"
+}
+
+variable "workload_network" {
+  description = "Porgroup to which the virtual machine will be connected."
+}
+variable "iscsi_network1" {
+  description = "Porgroup to which the virtual machine will be connected."
+}
+variable "iscsi_network2" {
+  description = "Porgroup to which the virtual machine will be connected."
+}
+variable "mgt_network" {
+  description = "Porgroup to which the virtual machine management will be connected."
+}
+
+# Indicate VM names and value of IP address last octet . By default it will create 5 VMs 
+variable "vm_names" {
+default = {
+  "cpf-esxi7.02a-101" = 101
+  "cpf-esxi7.02a-102" = 102
+  "cpf-esxi7.02a-103" = 103
+  
+
+}
+}
+
+
+variable "guest_vcpu" {
+  description = "Guest VM vCPU amount"
+}
+
+variable "guest_memory" {
+  description = "Guest VM Memory size"
+}
+
+variable "guest_user" {
+  description = "Username for guest VM."
+}
+variable "guest_password" {
+  description = "Password for guest user."
+}
+
+variable "guest_disk0_size" {
+  description = "Size of first disk to be added."   
+}
+
+variable "guest_dns" {
+  description = "DNS server for the guest."
+}
+variable "guest_ntp" {
+  description = "NTP server for the guest."  
+}
+variable "guest_domain" {
+  description = "Domain for the guest."  
+}
+variable "guest_start_ip" {
+  description = "Starting IP address for the guest vmk0 interface"  
+}
+variable "guest_start_ip1" {
+  description = "Starting IP address for the guest vmk1 interface"
+}
+variable "guest_start_ip2" {
+  description = "Starting IP address for the guest vmk2 interface"
+}
+variable "guest_netmask" {
+  description = "Netmask for the guest vmk0 interface"   
+}
+variable "guest_netmask1" {
+  description = "Netmask for the guest vmk1 interface"
+}
+variable "guest_netmask2" {
+  description = "Netmask for the guest vmk2 interface"
+}
+variable "guest_gateway" {
+  description = "Gateway for the guest vmk0 interface"   
+}
