0033a3ca073827431a6dbcc0272d5307de00cf80
oxteddybear <thebrownteddybear@gmail.com>
2021-07-11 22:42:52 +0800

    dsfg

--- workflow4/terraform/main.tf
+++ workflow4/terraform/main.tf
@@ -1,95 +1,30 @@
 provider "vsphere" {
-  user           = var.vsphere_user
-  password       = var.vsphere_password
-  vsphere_server = var.vsphere_server
+  user           = "administrator@vsphere.local"
+  password       = "VMware1!"
+  vsphere_server = "192.168.254.133"
   allow_unverified_ssl = true
 }
 
-resource "vsphere_datacenter" "target_dc" {
-  name = var.vsphere_datacenter
+data "vsphere_datacenter" "dc" {
+  name = "dc1"
 }
 
-######update this fingerprint block for as many 
-data "vsphere_host_thumbprint" "finger0" {
-  address = var.addhost.name[0]
-  insecure = true
+data "vsphere_datastore" "datastore" {
+  name          = "iscsi108"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_host_thumbprint" "finger1" {
-  address = var.addhost.name[1]
-  insecure = true
+data "vsphere_resource_pool" "pool" {
+  name          = "NSXT-EDGECLUSTER"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-resource "vsphere_compute_cluster" "c1" {
-  name            = var.compute_cluster
-  datacenter_id   = vsphere_datacenter.target_dc.moid
+data "vsphere_host" "host" {
+  name          = "192.168.254.108"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-locals {
-  fingerprint = [
-    data.vsphere_host_thumbprint.finger0.id,
-    data.vsphere_host_thumbprint.finger1.id
-  ]
-}
-
-resource "vsphere_host" "hostmember" {
-  count = length(var.addhost.name)
-  hostname = var.addhost.name[count.index]
-  username = var.esxi_user
-  password = var.esxi_password
-  thumbprint = local.fingerprint[count.index]
-  cluster = vsphere_compute_cluster.c1.id
-}
-
-
-#create mgt vds 
-resource "vsphere_distributed_virtual_switch" "vds1" {
-  name          = var.vds1_name
-  datacenter_id = vsphere_datacenter.target_dc.moid
-  max_mtu       = var.vds1_mtu
-  uplinks        = ["uplink1", "uplink2"]
-  
-  dynamic "host" {
-    for_each = vsphere_host.hostmember
-    content {
-      host_system_id = host.value.id #here host.value.id = <dynamic "host">."value" <==tis is a keyword to get the value id.<attribute> you can view the attribute in the state
-      devices        = var.mgt_vmnic
-    }
-  }
-}
-
-#create data vds
-resource "vsphere_distributed_virtual_switch" "vds2" {
-  name          = var.vds2_name
-  datacenter_id = vsphere_datacenter.target_dc.moid
-  max_mtu       = var.vds2_mtu
-  uplinks       = ["uplink1", "uplink2"]
-  
-  dynamic "host" {
-    for_each = vsphere_host.hostmember
-    content {
-      host_system_id = host.value.id #here host.value.id = <dynamic "host">."value" <==tis is a keyword to get the value id.<attribute> you can view the attribute in the state
-      devices        = var.data_vmnic
-    }
-  }
-}
-
-# Distributed port groups for access mgt network
-
-resource "vsphere_distributed_port_group" "pg1" {
-  for_each = var.pg1
-  name = each.key
-  distributed_virtual_switch_uuid = vsphere_distributed_virtual_switch.vds1.id
-  vlan_id = each.value
-}
-
-# #create pg on second vds - here i'm hardcoding since it makes no sense to create just 1 variable for this custom trunk
-resource "vsphere_distributed_port_group" "pg2" {
-  name     = "dpg-vdsdata-edgeuplink1"
-  distributed_virtual_switch_uuid = vsphere_distributed_virtual_switch.vds2.id
-
-    vlan_range { #got this of the state file
-        max_vlan = 4094
-        min_vlan = 0
-    }
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
