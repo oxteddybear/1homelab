f6f85f00a5b17bb9cf61dfff97f7cd8b7d3ad346
oxteddybear <thebrownteddybear@gmail.com>
2021-08-19 23:36:37 +0800

    Update main.tf

--- 5-add-interface-t0/main.tf
+++ 5-add-interface-t0/main.tf
@@ -43,13 +43,13 @@ data "nsxt_policy_edge_node" "edge02a" {
 # }
 
 resource "nsxt_policy_segment" "seg-uplink36" {
-  count=100
+  count=300
   display_name        = "seg-uplink36${count.index}"
   description         = "Terraform provisioned Segment"
   transport_zone_path = data.nsxt_policy_transport_zone.nsx-overlay-transportzone.path
 }
 resource "nsxt_policy_tier0_gateway_interface" "red_vrf_uplink1" {
-  count=100
+  count=300
   display_name   = "seg-uplink36${count.index}"
   type           = "EXTERNAL"
   edge_node_path = data.nsxt_policy_edge_node.edge01a.path
@@ -57,6 +57,19 @@ resource "nsxt_policy_tier0_gateway_interface" "red_vrf_uplink1" {
   segment_path   = nsxt_policy_segment.seg-uplink36[count.index].path
   # access_vlan_id = 112
   subnets        = ["192.168.${count.index}.1/31"]
+  mtu            = 1500
+
+  # depends_on = [nsxt_policy_tier0_gateway_interface.parent_uplink1]
+}
+resource "nsxt_policy_tier0_gateway_interface" "red_vrf_uplink1" {
+  count=300
+  display_name   = "seg-uplink36${count.index}"
+  type           = "EXTERNAL"
+  edge_node_path = data.nsxt_policy_edge_node.edge02a.path
+  gateway_path   = data.nsxt_policy_tier0_gateway.t0_red.path
+  segment_path   = nsxt_policy_segment.seg-uplink36[count.index].path
+  # access_vlan_id = 112
+  subnets        = ["192.168.${count.index}.3/31"]
   mtu            = 1500
 
   # depends_on = [nsxt_policy_tier0_gateway_interface.parent_uplink1]
