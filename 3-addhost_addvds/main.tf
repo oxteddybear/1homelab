d8ab152c15b47aa46b17d64d50dfcd74e8ceaba0
oxteddybear <thebrownteddybear@gmail.com>
2022-01-23 11:55:55 +0800

    adfasf

--- 3-addhost_addvds/main.tf
+++ 3-addhost_addvds/main.tf
@@ -1,28 +1,3 @@
-module "nsxv" {
-    source = "./nsxv" #directory! 
-    # Provider
-    vsphere_user = "administrator@vsphere.local"
-    vsphere_password = "VMware1!"
-    vsphere_server = "192.168.254.131"
-
-    # Infrastructure
-    vsphere_datacenter = "nsxv-dc"
-    compute_cluster = "compute-cluster"
-    esxi_user = "root"
-    esxi_password = "VMware1!"
-
-    ####vds stuff
-    vds1_name = "vdsmgt"
-    vds2_name = "vds-esxi-data"
-    
-
-    vds1_mtu = "1500"
-    vds2_mtu = "1700"
-    
-
-
-}
-
 module "nsxt" {
     source = "./nsxt"
     # Provider
