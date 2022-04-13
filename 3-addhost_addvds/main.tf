a997de8c619e662c531f346f6a5f1ab013a73aa7
ox-teddy <thebrownteddybear@gmail.com>
2022-04-13 10:51:44 +0000

    update 130

--- 3-addhost_addvds/main.tf
+++ 3-addhost_addvds/main.tf
@@ -3,7 +3,7 @@ module "nsxt" {
     # Provider
     vsphere_user = "administrator@vsphere.local"
     vsphere_password = "VMware1!"
-    vsphere_server = "192.168.254.131"
+    vsphere_server = "192.168.254.130"
 
     # Infrastructure
     vsphere_datacenter = "DC1"
