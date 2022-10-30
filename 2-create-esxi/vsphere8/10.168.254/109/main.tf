dcbdf0a112b0d313cee523d3e1d2b1dde4dbfdfd
teddybear <thebrownteddybear@gmail.com>
2022-10-31 00:12:33 +0800

    dd

--- 2-create-esxi/vsphere8/10.168.254/109/main.tf
+++ 2-create-esxi/vsphere8/10.168.254/109/main.tf
@@ -167,7 +167,7 @@ provisioner "remote-exec" {
     "esxcli iscsi networkportal add -n vmk1 -A vmhba65",
     "esxcli iscsi networkportal add -n vmk0 -A vmhba65",
     "esxcli iscsi adapter discovery sendtarget add -a 10.10.9.177:3260 -A vmhba65",
-    "esxcli iscsi adapter discovery sendtarget add -a 192.168.254.123:3260 -A vmhba65",
+    #"esxcli iscsi adapter discovery sendtarget add -a 192.168.254.123:3260 -A vmhba65",
     "esxcli iscsi adapter discovery rediscover -A vmhba65",
     ]
 }
