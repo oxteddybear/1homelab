782bf33a91506b7080d457098feee0d183633d8a
teddybear <thebrownteddybear@gmail.com>
2022-11-07 04:29:38 +0800

    Update var.tf

--- 3-addhost_addvds/vsphere7/109A/var.tf
+++ 3-addhost_addvds/vsphere7/109A/var.tf
@@ -14,8 +14,8 @@ variable "esxi_password"      {  description = "esxi root password"}
 variable "addhost" { ### compute hosts
   default = {
    name = [
-      "192.168.253.1",
-      "192.168.253.2"
+      "192.168.253.71",
+      "192.168.253.72"
     ]
   }
 }
